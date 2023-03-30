b321c7ae913f9e6d03ec2b677ee40bd4342742f1
PatKredatus <pat.kredatus@gmail.com>
2023-03-30 12:09:22 +0200

    modified logic

--- hangman_pygame/hangman_pygame.py
+++ hangman_pygame/hangman_pygame.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 import string
 
@@ -15,50 +17,48 @@ for i in range(7):
 
 # game variables
 words = ["car", "motorcycle", "glass", "hippo", "cheese", "python", "gameboy"]
-hangman_status = 0
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 FONT = pygame.font.Font(None, 36)
-
-guess = ""
 guide = FONT.render("Insert character: ", True, BLACK)
 guide2 = FONT.render("Guesses so far: ", True, BLACK)
-guess_surface = FONT.render(guess, True, BLACK)
-list_guesses_so_far = []
-guesses_string = ", ".join(list_guesses_so_far)
-guesses_surface = FONT.render(guesses_string, True, BLACK)
 
+while True:
+    random_word = random.choice(words)
+    hangman_status = 0
+    list_guesses_so_far = []
+    guesses_string = ", ".join(list_guesses_so_far)
+    guesses_surface = FONT.render(guesses_string, True, BLACK)
+    guess = ""
+    guess_surface = FONT.render(guess, True, BLACK)
 
-run = True
+    while hangman_status < 6:
+        win.fill(WHITE)
+        win.blit(images[hangman_status], (300, 20))
+        win.blit(guide, (10, 400))
+        win.blit(guide2, (10, 450))
 
-while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            elif event.type == pygame.KEYDOWN:
+                if event.unicode in string.ascii_letters and len(guess) < 1:
+                    guess += event.unicode
+                if event.key == pygame.K_BACKSPACE:
+                    guess = guess[:-1]
+                elif event.key == pygame.K_RETURN:
+                    if guess == "" or guess in list_guesses_so_far:
+                        continue
+                    else:
+                        if guess not in random_word:
+                            hangman_status += 1
+                        list_guesses_so_far.append(guess)
+                        guess = ""
+                        guesses_string = ", ".join(list_guesses_so_far)
+                        guesses_surface = FONT.render(guesses_string, True, BLACK)
 
-    win.fill(WHITE)
-    win.blit(images[hangman_status], (300, 20))
-    win.blit(guide, (10, 400))
-    win.blit(guide2, (10, 450))
+                guess_surface = FONT.render(guess, True, (0, 0, 0))
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        elif event.type == pygame.KEYDOWN:
-            if event.unicode in string.ascii_letters and len(guess) < 1:
-                guess += event.unicode
-            if event.key == pygame.K_BACKSPACE:
-                guess = guess[:-1]
-            elif event.key == pygame.K_RETURN:
-                if guess == "" or guess in list_guesses_so_far:
-                    continue
-                else:
-                    list_guesses_so_far.append(guess)
-                    guess = ""
-                    guesses_string = ", ".join(list_guesses_so_far)
-                    guesses_surface = FONT.render(guesses_string, True, BLACK)
-
-            guess_surface = FONT.render(guess, True, (0, 0, 0))
-
-    win.blit(guess_surface, (220, 400))
-    win.blit(guesses_surface, (220, 450))
-    pygame.display.update()
-
-pygame.quit()
+        win.blit(guess_surface, (220, 400))
+        win.blit(guesses_surface, (220, 450))
+        pygame.display.update()
