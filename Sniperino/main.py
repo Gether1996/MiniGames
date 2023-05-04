7cd24555a1438829d3dbba748dd09e0a9b8a11c4
PatKredatus <pat.kredatus@gmail.com>
2023-05-04 12:47:50 +0200

    added sound options, reworked screen size + size of objects and added game bar

--- Sniperino/main.py
+++ Sniperino/main.py
@@ -3,11 +3,12 @@ import random
 
 pygame.init()
 pygame.mixer.init()
-screen = pygame.display.set_mode((1500, 850))
+screen = pygame.display.set_mode((1500, 900))
 pygame.display.set_caption("Sniperino")
 clock = pygame.time.Clock()
 
-shot_sound = pygame.mixer.Sound('shot_sound.mp3')
+# sounds
+bullet_sound = pygame.mixer.Sound('shot_sound.mp3')
 fire_shot_sound = pygame.mixer.Sound('fire_shot_sound.mp3')
 
 # images
@@ -18,14 +19,21 @@ zombie2 = pygame.image.load('images/zombie2.png').convert_alpha()
 zombie_boss = pygame.image.load('images/zombie_boss.png').convert_alpha()
 bullet = pygame.image.load('images/power_bullet.png').convert_alpha()
 fire_bullet = pygame.image.load('images/flame_shot.png').convert_alpha()
+sound_on = pygame.image.load('images/sound_on.png').convert_alpha()
+sound_off = pygame.image.load('images/sound_off.png').convert_alpha()
 
-# positions
-sniper_position_y = 30
-bullet_position_x = 195
-bullet_position_y = sniper_position_y + 40
+# positions and rectangles
+sniper_position_y = 20
+bullet_position_x = 160
+UI_rect = pygame.Rect(0, 700, 1500, 200)
 
-random_y_position_for_zombie = [30, 190, 350, 510, 670]
 
+# colors
+BROWN = (205, 133, 63)
+
+random_y_position_for_zombie = [20, 160, 300, 440, 580]
+
+# new zombie1 added to game each 30 seconds
 ADD_ZOMBIE_EVENT = pygame.USEREVENT + 1
 pygame.time.set_timer(ADD_ZOMBIE_EVENT, 30000)
 
@@ -41,6 +49,7 @@ class Zombie:
             self.hit_points = 4
         else:
             self.rect = self.name_image.get_rect(topleft=(2000, random.choice(random_y_position_for_zombie[0:4])))
+            self.rect = self.rect.inflate(-30, -30)
             self.hit_points = 12
 
 
@@ -49,38 +58,42 @@ class Bullet:
         self.name_image = name_image
         if self.name_image == bullet:
             self.damage = 1
-            self.rect = self.name_image.get_rect(center=(bullet_position_x, sniper_position_y + 45))
+            self.rect = self.name_image.get_rect(center=(bullet_position_x, sniper_position_y + 40))
         elif self.name_image == fire_bullet:
             self.damage = 4
-            self.rect = self.name_image.get_rect(center=(bullet_position_x, sniper_position_y + 45))
+            self.rect = self.name_image.get_rect(center=(bullet_position_x, sniper_position_y + 40))
 
 
 zombies = []
-
+# creating starting zombies
 for i in range(3):
     zombie = Zombie(zombie1)
+    zombie.rect.x += 100*i
     zombies.append(zombie)
 for x in range(2):
     zombie = Zombie(zombie2)
+    zombie.rect.x += 150*x
     zombies.append(zombie)
-zombie = Zombie(zombie_boss)
-zombies.append(zombie)
+zombie_boss1 = Zombie(zombie_boss)
+zombies.append(zombie_boss1)
 
 num_of_bullets = 0
 bullets = []
+sound_turned_on = True
 
 while True:
     sniper_rect = sniper.get_rect(topleft=(30, sniper_position_y))
+    sound_rect = sound_on.get_rect(topleft=(50, 760))
     velocity = 0.2
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP and sniper_position_y > 160:
-                sniper_position_y -= 160
-            if event.key == pygame.K_DOWN and sniper_position_y < 650:
-                sniper_position_y += 160
+            if event.key == pygame.K_UP and sniper_position_y > 140:
+                sniper_position_y -= 140
+            if event.key == pygame.K_DOWN and sniper_position_y < 550:
+                sniper_position_y += 140
             if event.key == pygame.K_SPACE:
                 num_of_bullets += 1
                 if num_of_bullets % 10 == 0:
@@ -88,7 +101,7 @@ while True:
                     fire_shot_sound.play()
                     bullets.append(new_bullet)
                 else:
-                    shot_sound.play()
+                    bullet_sound.play()
                     new_bullet = Bullet(bullet)
                     bullets.append(new_bullet)
 
@@ -96,8 +109,25 @@ while True:
             zombie = Zombie(zombie1)
             zombies.append(zombie)
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = pygame.mouse.get_pos()
+            if sound_rect.collidepoint(event.pos) and sound_turned_on:
+                sound_turned_on = False
+            else:
+                sound_turned_on = True
+
     screen.blit(background, (0, 0))
+    pygame.draw.rect(screen, BROWN, UI_rect)
     screen.blit(sniper, (30, sniper_position_y))
+
+    if sound_turned_on:
+        screen.blit(sound_on, sound_rect)
+        bullet_sound.set_volume(0.4)
+        fire_shot_sound.set_volume(0.6)
+    else:
+        screen.blit(sound_off, sound_rect)
+        bullet_sound.set_volume(0)
+        fire_shot_sound.set_volume(0)
 
     for bullet_in_list in bullets:
         bullet_in_list.rect.x += 15
@@ -110,13 +140,16 @@ while True:
                     velocity += 0.1
                     if zombie.name_image == zombie1:
                         zombie.rect.x = 1600
+                        zombie.rect.y = random.choice(random_y_position_for_zombie)
                         zombie.hit_points = 2
                     elif zombie.name_image == zombie2:
                         zombie.rect.x = 1800
+                        zombie.rect.y = random.choice(random_y_position_for_zombie)
                         zombie.hit_points = 4
                     else:
                         zombie.rect.x = 2200
-                        zombie.hit_points = 20
+                        zombie.rect.y = random.choice(random_y_position_for_zombie[0:4])
+                        zombie.hit_points = 12
 
     for zombie in zombies:
         velocity += 0.2
